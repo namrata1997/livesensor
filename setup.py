44791cc82b3645da51880dd1bf31892e6d379e2e
Namrata Atri <namrata@Namratas-MacBook-Air.local>
2025-05-17 16:06:34 +0530

    after adding descriptione

--- setup.py
+++ setup.py
@@ -15,5 +15,6 @@ setup(
     author_email="atri.namrata06@gmail.com",
     packages = find_packages() ## finds all packages in the current directory i.e.__init__.py
     install_requires =  get_requirements()#['pymongo'],
+    description = "This is a sensor package"
 
 )
